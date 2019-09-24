38d4169a6cf8bfdcb97e8791ec1c6e92739ccd3c
unknown <rafaljuraszek@interia.pl>
2019-09-24 15:05:43 +0200

    Added Burger page

---
+++ src/components/Burger/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+const test = (props) =>
+(
+    <h1> <BurgerIngredient /></h1>
+    
+);
+
+
+export default test;
+
